61e609d120f0e862b4dfc67d83e53272537ddd3b
Jessie Donovan <donovanajessie@gmail.com>
2020-06-23 02:59:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do |welome|
+  assign_rooms(attendees).each do |welcome|
 end
     puts welcome
 
